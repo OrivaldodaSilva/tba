e59fa63afe2f2462af2b218e80b7e37dae30422b
OrivaldodaSilva <100154884+OrivaldodaSilva@users.noreply.github.com>
2023-02-06 15:03:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBAaHSQzmyd4AHOQHgzWtnUWYL-HtETRaE",
+    authDomain: "spectagram-e9f94.firebaseapp.com",
+    databaseURL: "https://spectagram-e9f94-default-rtdb.firebaseio.com",
+    projectId: "spectagram-e9f94",
+    storageBucket: "spectagram-e9f94.appspot.com",
+    messagingSenderId: "551804696526",
+    appId: "1:551804696526:web:5fe9fa63ec4f8918245d9f"
+  };
